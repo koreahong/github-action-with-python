6003bdc3bb4de3c04ee2596a54df29d1cf712c1e
koreahong <51938331+koreahong@users.noreply.github.com>
2021-10-20 18:50:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+from datetime import datetime
+from pytz import timezone
+from crawling_yes24 import parsing_beautifulsoup, extract_book_data
+from github_utils import get_github_repo, upload_github_issue
+
 if __name__ == "__main__":
     access_token = os.environ['MY_GITHUB_TOKEN']
     repository_name = "github-action-with-python"
